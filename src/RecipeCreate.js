11f5d24e10be0cdf9c3fbd792dbb5a9006e79c64
Sammy Ali <sammyali11.19@gmail.com>
2023-09-12 10:41:02 -0400

    Created initialState for form. Created handleChanges function for form as well as handleSubmit. Added RecipeView Component

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,51 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({ createRecipe }) {
+  const initialState = {
+    name: "",
+    cuisine: "",
+    url: "",
+    ingredients: "",
+    preparation: "",
+  }
 
+  const [formData, setFormData] = useState(initialState)
+
+  const handleChanges = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    })
+  }
+
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData)
+  }
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={handelSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+          <td>
+                <input id="name" name="name" type="text" value={formData.name} onChange={handleChanges}/>
+            </td>
+          <td>
+                <input id="cuisine" name="cuisine" type="text" value={formData.cuisine} onChange={handleChanges}/>
+            </td>
+            <td>
+                <input id="url" name="url" type="text" value={formData.url} onChange={handleChanges}/>
+            </td>
+            <td>
+                <textarea id="ingredients" name="ingredients" value={formData.ingredients} onChange={handleChanges}></textarea>
+            </td>
+            <td>
+                <textarea id="preparation" name="preparation" value={formData.preparation} onChange={handleChanges}></textarea>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
